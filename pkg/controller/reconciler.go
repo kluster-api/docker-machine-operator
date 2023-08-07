01f1a711fa57c3a0aa0eb1187c0e322b8e7b6207
Shaad7 <abdullah.alshaad@appscode.com>
2023-08-07 19:55:51 +0600

    Update conditions
    Signed-off-by: Shaad7 <abdullah.alshaad@appscode.com>

--- pkg/controller/reconciler.go
+++ pkg/controller/reconciler.go
@@ -5,8 +5,10 @@ import (
 	"encoding/base64"
 	"fmt"
 	"github.com/appscode/go/crypto/rand"
+	api "go.klusters.dev/docker-machine-operator/api/v1alpha1"
 	core "k8s.io/api/core/v1"
 	kmapi "kmodules.xyz/client-go/api/v1"
+	cutil "kmodules.xyz/client-go/conditions"
 	"os"
 	"os/exec"
 )
@@ -16,14 +18,19 @@ const scriptFileDirectory = "/tmp/"
 func (r *MachineReconciler) createMachine(driverName string) error {
 	r.log.Info("Creating Google Compute Engine")
 	args, err := r.getMachineCreationArgs(driverName)
-
+	if err != nil {
+		return err
+	}
+	cutil.MarkTrue(r.machineObj, api.MachineConditionMachineCreating)
 	cmd := exec.Command("docker-machine", args...)
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println("Could not create machine")
-		fmt.Println(err.Error())
+		r.log.Error(err, "can not create machine")
+		cutil.MarkFalse(r.machineObj, api.MachineConditionTypeMachineReady, err.Error(), kmapi.ConditionSeverityError,
+			"Unable to create docker machine", driverName)
 		return err
 	}
+	cutil.MarkTrue(r.machineObj, api.MachineConditionTypeMachineReady)
 	r.log.Info("Created Docker Machine Successfully")
 	return nil
 }
@@ -39,14 +46,20 @@ func (r *MachineReconciler) getMachineCreationArgs(driverName string) ([]string,
 
 	scriptArgs, err := r.getStartupScriptArgs()
 	if err != nil {
+		r.log.Error(err, "unable to create script")
+		cutil.MarkFalse(r.machineObj, api.MachineConditionTypeScriptReady, err.Error(), kmapi.ConditionSeverityError, "unable to create script", scriptArgs)
 		return nil, err
 	}
+	cutil.MarkTrue(r.machineObj, api.MachineConditionTypeScriptReady)
 	args = append(args, scriptArgs...)
 
 	authArgs, err := r.getAuthSecretArgs()
 	if err != nil {
+		r.log.Error(err, "unable to read auth data")
+		cutil.MarkFalse(r.machineObj, api.MachineConditionTypeAuthDataReady, err.Error(), kmapi.ConditionSeverityError, "unable to read auth data", authArgs)
 		return nil, err
 	}
+	cutil.MarkTrue(r.machineObj, api.MachineConditionTypeAuthDataReady)
 	args = append(args, authArgs...)
 	args = append(args, r.machineObj.Name)
 	return args, nil
@@ -61,6 +74,7 @@ func (r *MachineReconciler) getAuthSecretArgs() ([]string, error) {
 	for key, value := range authSecret.Data {
 		data := base64.StdEncoding.EncodeToString(value)
 		if len(data) == 0 || len(key) == 0 {
+			cutil.MarkTrue(r.machineObj, api.MachineConditionAuthDataNotFound)
 			return nil, fmt.Errorf("auth secret not found")
 		}
 		authArgs = append(authArgs, fmt.Sprintf("--%s", key))
@@ -88,6 +102,7 @@ func (r *MachineReconciler) getStartupScriptArgs() ([]string, error) {
 		}
 	}
 	if len(userDataKey) == 0 || len(userDataValue) == 0 {
+		cutil.MarkTrue(r.machineObj, api.MachineConditionScriptDataNotFound)
 		return nil, fmt.Errorf("script data not found")
 	}
 	err = os.WriteFile(filePath, []byte(userDataValue), 0644)
